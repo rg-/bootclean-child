6cbf80916996c190d515ca2b19fa6a9450804a6a
rg- <roberto@rgdesign.org>
2020-07-21 01:38:15 -0300

    Addons
    - Private Areas
    - Site settings
    - Moved to version 11.0.1 as parent

--- functions.php
+++ functions.php
@@ -1,37 +1,84 @@
 <?php
+/* ################################################################################## */
+/* ################################################################################## */
+/**
+ * Bootclean child custom functions
+ *
+ * @package Bootclean
+ * @subpackage Child Theme
+ * 
+ */
+/* ################################################################################## */
+/* ################################################################################## */
 
-/*
+/**
+ * @subpackage Enable "theme_settings" options pages
+ */
 
-	Bootclean child custom functions 
+	add_filter('wpbc/filter/theme_settings/installed', '__return_true');
+		
+		/* Customs for theme settings here */
+		
+		include('functions/addon-theme_settings.php');
 
-*/
+/* ################################################################################## */
 
-	/*
-	Enable Theme Settings Page
-	*/
+/**
+ * @subpackage Enable "swup" addon
+ */
 
-/* Enable theme_settings addon */
-add_filter('wpbc/filter/theme_settings/installed', '__return_true');
-include('functions/theme-options-theme-settings.php');
+	add_filter('wpbc/filter/swup/installed', '__return_true');
+	// include('functions/addon-swup.php');
 
-/* Enable private_areas addon */
-add_filter('wpbc/filter/private_areas/installed', '__return_true');
-include('functions/core-private_areas.php');
+/* ################################################################################## */
 
-/* general options/settings */
-include('functions/theme-textdomain.php'); 
-include('functions/theme-options.php');
-include('functions/theme-options-page-settings.php');
-include('functions/theme-scripts.php');
-include('functions/theme-fonts.php');
-include('functions/theme-widgets.php');
+/**
+ * @subpackage Enable "private_areas" addon
+ */
+
+	// add_filter('wpbc/filter/private_areas/installed', '__return_true');
+	// include('functions/addon-private_areas.php');
+
+/* ################################################################################## */
+
+/**
+ * @subpackage "theme-*" customs
+ */
+
+	include('functions/theme-textdomain.php'); 
+	// include('functions/theme-options.php');
+	// include('functions/theme-options-page-settings.php');
+	// include('functions/theme-scripts.php');
+	// include('functions/theme-fonts.php');
+	// include('functions/theme-widgets.php');
+
+/* ################################################################################## */
 
 /* core */
-include('functions/core-theme_support.php'); 
+// include('functions/core-theme_support.php'); 
+
+/* ################################################################################## */
 
 /* front-end layout */ 
-include('functions/layout.php');
-include('functions/layout-navmenus.php');
+// include('functions/layout.php');
+// include('functions/layout-navmenus.php');
 
-/* plugins */
-include('functions/plugins-woocommerce.php');   
+/* ################################################################################## */
+
+/**
+ * @subpackage WooCommerce
+ */
+if( class_exists( 'WooCommerce' ) ){
+	include('functions/plugins-woocommerce.php');
+}
+
+/* ################################################################################## */
+
+
+/* ################################################################################## */
+
+
+/* ################################################################################## */
+
+
+/* ################################################################################## */
